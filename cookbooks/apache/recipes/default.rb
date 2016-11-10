1a3f03b2b46cad4cf4d174e69ac7a86388f9987e
tester <tester@workstation.r2.test>
2016-11-10 09:27:45 +0100

    add motd

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,10 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "httpd" do
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
+package "apache2" do
+	package_name package
 	action :install
 end
 
-service "httpd" do
+service "apache2" do
+	service_name package
 	action [:enable, :start]
 end
+
